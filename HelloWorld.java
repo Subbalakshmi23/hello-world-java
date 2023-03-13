0273c3cf9a81d34ccd3d9625ea2fb250cf610a31
Subbalakshmi23 <subbu502cse@gmail.com>
2023-03-13 12:20:22 +0000

    adding new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -34,6 +34,12 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
+System.out.println("Hello, World");
+System.out.println("Hello, World");
+System.out.println("Hello, World");
+System.out.println("Hello, World");
+System.out.println("Hello, World");
+System.out.println("Hello, World");
     System.out.println("Hello, World");
   }
 }
