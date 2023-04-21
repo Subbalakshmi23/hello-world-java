e2b1dd10d3e0942370a862bd7c1118587336f366
Subbalakshmi23 <subbu502cse@gmail.com>
2023-04-21 04:52:15 +0000

    addded new line

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,8 +35,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
    System.out.println("good morning");
-    System.out.println("good morning");
-    System.out.println("good morning");
-    System.out.println("good morning");
+   System.out.println("hi siva");
   }
 }
